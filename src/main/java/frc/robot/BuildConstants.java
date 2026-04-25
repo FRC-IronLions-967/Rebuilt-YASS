e896ca047b7349b924bc02520056578cd397c2ed
timothyscholl <timfoxgod@gmail.com>
2026-04-25 10:22:57 -0500

    Initial Commit
    WHEEEEEEEEEE

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "967_AdvantageKitTemplate_2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "f285ed0bf98d508a6beef2f57a71144c84a143dd";
+  public static final String GIT_DATE = "2025-11-22 12:31:21 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-11-22 15:27:29 EST";
+  public static final long BUILD_UNIX_TIME = 1763843249567L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
